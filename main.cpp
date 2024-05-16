ffc4c320370e35532a5123ae5b69f9909a91ccc7
efealgan <er.efesal@gmail.com>
2024-05-16 19:50:41 +0300

    added straight recognition through blood toil tears and sweat

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <random>
+#include <bits/stdc++.h> 
 
 /*  i = 0 -> spades
     i = 1 -> clubs
     i = 2 -> diamonds
     i = 3 -> hearts 
 */
-int cards[4][13];
-int dealtCards[4][13];
-int dealtSuit;
-int dealtRank;
-int dealtCard[2];
-bool firstRound = true;
-const int PLAYERCOUNT = 4;
-const int PLAYERMONEY = 1000;
-const int PLAYERBET = 0;
-int bustedPlayerCount = 0;
-int bufferSuit;
-int bufferRank;
+int cards[4][13];               //is cards
+int dealtCards[4][13];          //stores if a card is dealt or not. 0-> not dealt, 1-> dealt.
+int dealtSuit;                  //stores the most recently dealt (selected at random) suit
+int dealtRank;                  //stores the most recently dealt (selected at random) rank
+int dealtCard[2];               //combines the two values above, idk if this is needed
+bool firstRound = true;         //so we can have a gameloop i guess
+const int PLAYERCOUNT = 4;      //default playercount, note that changing this does not actually add new players. this is just for displaying player count.
+const int PLAYERMONEY = 1000;   //default starting money. 
+const int PLAYERBET = 0;        //default starting bet.
+int bustedPlayerCount = 0;      //is busted player count
+int bufferSuit;                 //idk where i used this
+int bufferRank;                 //same as bufferSuit
 
 
 void displayCard(int suit, int rank) {
@@ -77,7 +78,7 @@ int dealCard(int amount) {
         dealtCards[suit][rank] = 1;
         dealtSuit = suit;
         dealtRank = rank;
-        return suit, rank, dealCard(--amount); ///check if this is correct ///yeah i think this is correct
+        return suit, rank, dealCard(--amount); ///check if this is correct ///yeah i think this is correct //it does work, i just don't know how.
     }
     else {
         return 0;
@@ -90,7 +91,7 @@ void initCards() {
         }
     }
 }
-void listCards() {
+void listCards() { //this function is completely unnecessary and the symbols cannot be displayed outside vscode just yet. //but it stays c:
     for (int i = 0; i < 4; i++) {
         if (i == 0) {
             std::cout << "♠A ";
@@ -154,8 +155,12 @@ class Players {
         int bet;
         
     public:
-        int pWideHand[7][2];
-        int heldCards[2][2];
+        int handScore;              //may not use this.
+        int pWideHand[7][2];        //= community + player cards
+        int pShortHand[5][2];       //the best evaluated hand.
+        int pShortHandBuffer[5][2]; //the best evaluated hand 2: electric boogaloo because i am stupid
+        int heldCards[2][2];        //player cards
+        int flushCards[7][2];       //we pass this to straight() after a flush so we can check for a straight flush
         Players(int id, const int m, const int b) {
             playerID = id;
             money = PLAYERMONEY;
@@ -293,18 +298,49 @@ public:
         players[2].getHeldCards(3, true);
         players[3].getHeldCards(3, true);
         
-        std::cout << std::endl;
-        displayHand(0);
-        std::cout << std::endl;
-        displayHand(1);
-        std::cout << std::endl;
-        displayHand(2);
-        std::cout << std::endl;
-        displayHand(3);
-        std::cout << std::endl;
         
+        displayCommunityCards();
         scoreHands();
 
+    }
+    void debugMode() {
+        dealFirstCards();
+        updatePlayerHands();
+        setCommunityCards(0, 9, 2, 3, 0, 10, 0, 11, 0, 12);
+        players[0].getHeldCards(3, true);
+        players[1].getHeldCards(3, true);
+        players[2].getHeldCards(3, true);
+        players[3].getHeldCards(3, true);
+        
+        
+        displayCommunityCards();
+        scoreHands();
+    }
+    void setCommunityCards(int s1, int r1, int s2, int r2, int s3, int r3, int s4, int r4, int s5, int r5){
+        communityCards[0][0] = s1;
+        communityCards[0][1] = r1;
+        updatePlayerHands();
+        communityCards[1][0] = s2;
+        communityCards[1][1] = r2;
+        updatePlayerHands();
+        communityCards[2][0] = s3;
+        communityCards[2][1] = r3;
+        updatePlayerHands();
+        communityCards[3][0] = s4;
+        communityCards[3][1] = r4;
+        updatePlayerHands();
+        communityCards[4][0] = s5;
+        communityCards[4][1] = r5;
+        updatePlayerHands();
+        
+
+    }
+    void displayCommunityCards() {
+        for (int i = 0; i < 5; i++) {
+            std::cout << "Community card " << i + 1 << " is: ";
+            displayCard(communityCards[i][0], communityCards[i][1]);
+            std::cout << std::endl;
+        }
     }
     void dealCommunityCards(int amount) {
     for (int i = 0; i < amount; i++)
@@ -360,14 +396,127 @@ public:
 
     void scoreHands() {
         //TODO: implement scoring
+        for (int i = 0; i < 4; i++) {
+            flush(i);
+        }
+        for (int i = 0; i < 4; i++) {
+            straight(i, false);
+        }
     }
     int flush(int id) {
+        std::string flushSuit = "None";
+        int flushCounter = 0;
+        int Fspades = 0;
+        int Fclubs = 0;
+        int Fdiamonds = 0;
+        int Fhearts = 0;
+        for (int i = 0; i < 7; i++) {
+            switch (players[id].pWideHand[i][0])
+            {
+            case 0:
+                Fspades++;
+                break;
+            case 1:
+                Fclubs++;
+                break;
+            case 2:
+                Fdiamonds++;
+                break;
+            case 3:
+                Fhearts++;
+                break;
+            }
+        }
+        //choose the highest flush count
+        if (Fspades >= 5){
+            flushCounter = Fspades;
+            flushSuit = "Spades";
+        }
+        else if (Fclubs >= 5){
+            flushCounter = Fclubs;
+            flushSuit = "Clubs";
+        }
+        else if (Fdiamonds >= 5){
+            flushCounter = Fdiamonds;
+            flushSuit = "Diamonds";
+        }
+        else if (Fhearts >= 5){
+            flushCounter = Fhearts;
+            flushSuit = "Hearts";
+        }
+        
+        if (flushCounter >= 5) {
+            std::cout << "Player " << id + 1 << " has a flush of " << flushSuit << "!\n";
+            return 1;
+        }
+        else {
+            std::cout << "Player " << id + 1 << " does not have a flush.\n";
+            return 0;
+        }
         bool flush = false;
         return flush;
     }
-    int straight(int id) {
+    int straight(int id, bool shortHand = false) {
+        int sCounter = 1;
         bool straight = false;
-        return straight;
+        int currentStraight[5] = {-1, -1, -1, -1, -1}; 
+        if (shortHand == false){        
+            int ranks[7];
+            for (int i = 0; i < 7; i++) {
+                ranks[i] = players[id].pWideHand[i][1];
+            }
+            int n = sizeof(ranks) / sizeof(ranks[0]);
+            std::sort(ranks, ranks + n, std::greater<int>());
+            currentStraight[0] = ranks[0];
+            for (int i = 0; i < 7; i++) {
+                if ((currentStraight[0] == 12) && (currentStraight[1] == 11) && (currentStraight[2] == 10) && (currentStraight[3] == 9)){ //the current hand has a chance to be a broadway.
+                    for (int j = i; j < 7; j++ ){
+                        if (ranks[j] == 0){
+                            std::cout << "Player " << id + 1 << " has a Broadway Straight!" << std::endl;
+                            straight = true;
+                            break;
+                        }
+                    }
+                }
+                if(straight){
+                    break;
+                }
+                
+                else if (ranks[i] - ranks[i + 1] == 1) {                            //PROBABLY REMOVE i - i+1 = 9 part is necessary for broadway hand recognition. although the problem with that is ace will be the last card on the hand. so it will most likely not be neighboring the 10. 
+                    currentStraight[sCounter] = ranks[i+1];
+                    sCounter++;                                                     // one solution might be to check if the ongoing straight is a K-Q-J-10. If so, we would just check for an Ace in the hand and if it returns true, the player has a broadway straight.
+                    if (sCounter >= 5){                                             //check if the player just completed the straight, so we don't keep checking unnecessarily.
+                        straight = true;                                            //also the next check might reset the counter. so this is necessary
+                        break;
+                    }
+                }
+                else if (ranks[i] - ranks[i + 1] == 0) {                            //the straight hasn't broken yet but didn't continue either. so we don't touch it.
+                    //wtf is this shit
+                    //maybe should've used a switch
+                    //anyways do nothing so maybe the straight will continue
+                    //TODO: commit suicide
+                }
+                else {
+                    //reset the counter and currentStraight
+                    for (int j = 0; j < 5; j++){
+                        currentStraight[j] = -1;
+                    }
+                    sCounter = 1;
+                }
+            }
+            if (straight){
+                std::cout << "Player " << id + 1 << " has a straight!\n";
+            }
+            else{
+                std::cout << "Player " << id + 1 << " does not have a straight.\n";
+            }
+        return 0;
+    }
+        else {
+            int ranks[7];
+
+        }
+        return 0;
     }
     int duplicate(int id) {
         bool duplicate = false;
@@ -415,8 +564,8 @@ int main() {
     listCards();
 
     Game game;
-    game.gameLoop();
-    
+    //game.gameLoop();
+    game.debugMode();
     std::cout << "\nPress Enter to quit.";
     std::cin.get();
     return 0;
